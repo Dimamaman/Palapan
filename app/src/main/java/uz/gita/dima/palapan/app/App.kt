b7d1e94b976fdf8b79b6768ddf2b60a1675fd51d
dinmuxammedmamanov <dimamanov37@gmail.com>
2023-05-03 23:24:48 +0500

    finish

--- app/src/main/java/uz/gita/dima/palapan/app/App.kt
+++ app/src/main/java/uz/gita/dima/palapan/app/App.kt
@@ -1,11 +1,11 @@
 package uz.gita.dima.palapan.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App : Application() {
-
     companion object {
         lateinit var instance: App
             private set
@@ -13,6 +13,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         instance = this
     }
 }
